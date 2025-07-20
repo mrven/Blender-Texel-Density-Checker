029958e5a20e927dd2442ce6493c1019c1b15827
Ivan Vostrikov <mrven@mail.ru>
2025-07-20 11:33:24 +0300

    added tests for check td (Python backend base check, "Selected Faces disabled")

--- Texel_Density_2025_1_Bl420/tests/test_check_operator.py
+++ Texel_Density_2025_1_Bl420/tests/test_check_operator.py
@@ -3,44 +3,7 @@ import unittest
 from .. import utils
 
 class TestTexelDensityCheckOperator(unittest.TestCase):
-	# TODO: Добавлять в сцену на старте объекта, чтобы была возможность тестить как один, так и несколько объектов
-	# Возможно даже стоит добавить что-то из НЕ мешей и перебирать в тестах разные варианты активного объекта
-
-	def setUp(self):
-		print(f"Current backend: {utils.get_preferences().calculation_backend}")
-
-		if bpy.context.object and bpy.context.object.mode != 'OBJECT':
-			try:
-				bpy.ops.object.mode_set(mode='OBJECT')
-			except Exception:
-				pass
-		# Удаляем все объекты со сцены
-		for obj in bpy.data.objects:
-			obj.select_set(True)
-		bpy.ops.object.delete()
-
-		# Создаём новый куб с UV
-		bpy.ops.mesh.primitive_cube_add(enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 2, 1))
-		self.obj = bpy.context.active_object
-
-		# Переходим в OBJECT режим
-		bpy.ops.object.mode_set(mode='OBJECT')
-
-	def tearDown(self):
-		# Всегда выйти из edit-mode, даже если тест неудачный
-		if bpy.context.object and bpy.context.object.mode != 'OBJECT':
-			try:
-				bpy.ops.object.mode_set(mode='OBJECT')
-			except Exception:
-				pass
-
-		# Удаляем все объекты после теста, чтобы чисто
-		for obj in bpy.data.objects:
-			obj.select_set(True)
-		bpy.ops.object.delete()
-
-		print(" ")
-
+	# TODO: Возможно даже стоит добавить что-то из НЕ мешей и перебирать в тестах разные варианты активного объекта
 	# Тест кейсы:
 	# • в обжект моде 3д вью
 	# • в эдит моде 3д вью с/без селектед фейсес
@@ -55,6 +18,45 @@ class TestTexelDensityCheckOperator(unittest.TestCase):
 	# • кейс, когда нет активного объекта
 
 	#  Лучше всего тесты держать атомарными и делать по одной проверке в функции, чтобы не повышать сложность тестов
+
+	# All Tests start in object mode
+	def setUp(self):
+		print(f"Current backend: {utils.get_preferences().calculation_backend}")
+
+		if bpy.context.object and bpy.context.object.mode != 'OBJECT':
+			try:
+				bpy.ops.object.mode_set(mode='OBJECT')
+			except Exception:
+				pass
+
+		for obj in bpy.data.objects:
+			obj.select_set(True)
+		bpy.ops.object.delete()
+
+		# Added UV Sphere
+		bpy.ops.mesh.primitive_uv_sphere_add(radius=1, enter_editmode=False, align='WORLD', location=(2, 0, 0), scale=(2, 2, 2))
+		self.sphere = bpy.context.active_object
+
+		# Added Cube
+		bpy.ops.mesh.primitive_cube_add(enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 2, 1))
+		self.cube = bpy.context.active_object
+
+		bpy.ops.object.mode_set(mode='OBJECT')
+
+	def tearDown(self):
+		# Always return to Object Mode
+		if bpy.context.object and bpy.context.object.mode != 'OBJECT':
+			try:
+				bpy.ops.object.mode_set(mode='OBJECT')
+			except Exception:
+				pass
+
+		# Clean up scene
+		for obj in bpy.data.objects:
+			obj.select_set(True)
+		bpy.ops.object.delete()
+
+		print(" ")
 
 	# TD Check Single Selected Object in Object Mode
 	def test_check_object_mode(self):
@@ -72,11 +74,14 @@ class TestTexelDensityCheckOperator(unittest.TestCase):
 
 		td = bpy.context.scene.td
 		bpy.ops.object.mode_set(mode='OBJECT')
+		bpy.ops.object.select_all(action='DESELECT')
+		bpy.context.view_layer.objects.active = self.cube
+		bpy.context.view_layer.objects.active.select_set(True)
 
 		expected_density = '0.515'
 		expected_uv_space = '37.5000 %'
 
-		for i in range(3):  # Проверим трижды подряд
+		for i in range(3):  # Three times check
 			with self.subTest(cycle=i):
 				try:
 					td.density = ''
@@ -112,6 +117,12 @@ class TestTexelDensityCheckOperator(unittest.TestCase):
 			original_type = None
 
 		td = bpy.context.scene.td
+
+		bpy.ops.object.mode_set(mode='OBJECT')
+		bpy.ops.object.select_all(action='DESELECT')
+		bpy.context.view_layer.objects.active = self.cube
+		bpy.context.view_layer.objects.active.select_set(True)
+
 		obj = bpy.context.active_object
 
 		td.density = ''
@@ -158,6 +169,112 @@ class TestTexelDensityCheckOperator(unittest.TestCase):
 		if original_type:
 			area.type = original_type
 
+	# Base Check for Py Backend
+	def test_check_py_back(self):
+		print("----Run Test Base Check for Py Backend---")
+
+		utils.get_preferences().calculation_backend = 'PY'
+		print(f"Backend switched to Py. Current is: {utils.get_preferences().calculation_backend}")
+
+		area = bpy.context.area
+		if not area:
+			self.fail("Failed get area")
+
+		if area.type != 'VIEW_3D':
+			original_type = area.type
+			area.type = 'VIEW_3D'
+		else:
+			original_type = None
+
+		td = bpy.context.scene.td
+		bpy.ops.object.mode_set(mode='OBJECT')
+		bpy.ops.object.select_all(action='DESELECT')
+		bpy.context.view_layer.objects.active = self.cube
+		bpy.context.view_layer.objects.active.select_set(True)
+
+		expected_density = '0.515'
+		expected_uv_space = '37.5000 %'
+
+		for i in range(3):  # Three times check
+			with self.subTest(cycle=i):
+				try:
+					td.density = ''
+					td.uv_space = ''
+
+					result = bpy.ops.texel_density.check()
+
+					print(f"Run {i + 1}: density={td.density}, uv_space={td.uv_space}")
+
+					self.assertIn('FINISHED', result)
+					self.assertEqual(td.density, expected_density)
+					self.assertEqual(td.uv_space, expected_uv_space)
+				except Exception as e:
+					self.fail(f"Test failed on cycle try {i} with exception: {e}")
+				finally:
+					bpy.ops.object.mode_set(mode='OBJECT')
+
+		utils.get_preferences().calculation_backend = 'CPP'
+		print(f"Backend switched to CPP. Current is: {utils.get_preferences().calculation_backend}")
+
+		if original_type:
+			area.type = original_type
+
+	# TD Check Single Selected Object in Edit Mode with disabled Selected Faces option
+	def test_check_edit_mode_no_selected_faces(self):
+		print("----Run Test TD Check (in Edit Mode / No Selected Faces)---")
+
+		area = bpy.context.area
+		if not area:
+			self.fail("Failed get area")
+
+		if area.type != 'VIEW_3D':
+			original_type = area.type
+			area.type = 'VIEW_3D'
+		else:
+			original_type = None
+
+		td = bpy.context.scene.td
+
+		bpy.ops.object.mode_set(mode='OBJECT')
+		bpy.ops.object.select_all(action='DESELECT')
+		bpy.context.view_layer.objects.active = self.cube
+		bpy.context.view_layer.objects.active.select_set(True)
+
+		td.density = ''
+		td.uv_space = ''
+
+		td.selected_faces = False
+
+		expected_density = '0.515'
+		expected_uv_space = '37.5000 %'
+
+		for i in range(3):  # Three times check
+			with self.subTest(cycle=i):
+				try:
+					td.density = ''
+					td.uv_space = ''
+
+					bpy.ops.object.mode_set(mode='EDIT')
+					bpy.ops.mesh.select_all(action='DESELECT')
+
+					result = bpy.ops.texel_density.check()
+
+					print(f"Run {i + 1}: density={td.density}, uv_space={td.uv_space}")
+
+					bpy.ops.object.mode_set(mode='OBJECT')
+
+					self.assertIn('FINISHED', result)
+					self.assertEqual(td.density, expected_density)
+					self.assertEqual(td.uv_space, expected_uv_space)
+				except Exception as e:
+					self.fail(f"Test failed on cycle try {i} with exception: {e}")
+				finally:
+					bpy.ops.object.mode_set(mode='OBJECT')
+
+		td.selected_faces = True
+
+		if original_type:
+			area.type = original_type
 
 def run():
 	suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestTexelDensityCheckOperator)
