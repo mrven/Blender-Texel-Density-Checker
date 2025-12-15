31bec154e4e18aa8538aef2e434b705cb1c3a818
Ivan Vostrikov <mrven@mail.ru>
2025-12-15 23:05:57 +0300

    start developing 2025.1.2

---
+++ texel_density_2025_1_2/constants.py
@@ -0,0 +1,174 @@
+# Asset Names
+TD_VC_NAME = 'td_vis'
+TD_MATERIAL_NAME = 'TD_Checker'
+
+
+# URLs
+TD_DOC_URL = 'https://github.com/mrven/Blender-Texel-Density-Checker/blob/master/README.md#documentation'
+TD_REPORT_URL = 'https://github.com/mrven/Blender-Texel-Density-Checker/blob/master/docs/bug_report.md'
+
+
+# Enums/Dictionaries/Lists
+TD_PRESET_VALUES = {
+    # px/cm
+    '0': [
+        ["20.48", "10.24", "5.12"],
+        ["2.56", "1.28", "0.64"]
+    ],
+    #px/m
+    '1': [
+        ["2048", "1024", "512"],
+        ["256", "128", "64"]
+    ],
+    #px/in
+    '2': [
+        ["52.019", "26.01", "13.005"],
+        ["6.502", "3.251", "1.626"]
+    ],
+    # px/ft
+    '3': [
+        ["624.23", "312.115", "156.058"],
+        ["78.029", "39.014", "19.507"]
+    ]
+}
+
+TD_ANCHOR_ORIGIN_ITEMS = (('SELECTION', 'Selection', ''),
+                          ('UV_CENTER', 'UV Center', ''),
+                          ('UV_LEFT_BOTTOM', 'UV Left Bottom', ''),
+                          ('UV_LEFT_TOP', 'UV Left Top', ''),
+                          ('UV_RIGHT_BOTTOM', 'UV Right Bottom', ''),
+                          ('UV_RIGHT_TOP', 'UV Right Top', ''),
+                          ('2D_CURSOR', '2D Cursor', ''))
+
+TD_TEXTURE_SIZE_ITEMS = (('512', '512px', ''),
+                        ('1024', '1024px', ''),
+                        ('2048', '2048px', ''),
+                        ('4096', '4096px', ''),
+                        ('CUSTOM', 'Custom', ''))
+
+TD_UNITS_ITEMS = (('0', 'px/cm', ''),
+				  ('1', 'px/m', ''),
+				  ('2', 'px/in', ''),
+				  ('3', 'px/ft', ''))
+
+TD_SET_METHOD_ITEMS = (('EACH', 'Each', ''),
+                       ('AVERAGE', 'Average', ''))
+
+TD_CHECKER_METHOD_ITEMS = (('REPLACE', 'Replace', ''),
+                           ('STORE', 'Store and Replace', ''))
+
+TD_CHECKER_TYPE_ITEMS = (('COLOR_GRID', 'Color Grid', ''),
+                         ('UV_GRID', 'UV Grid', ''))
+
+TD_BAKE_VC_MODE_ITEMS = (('TD_FACES_TO_VC', 'Texel (by Face)', ''),
+						 ('TD_ISLANDS_TO_VC', 'Texel (by Island)', ''),
+						 ('UV_ISLANDS_TO_VC', 'UV Islands', ''),
+						 ('UV_SPACE_TO_VC', 'UV Space (%)', ''),
+						 ('DISTORTION', 'UV Distortion', ''))
+
+TD_BAKE_UV_ISLANDS_MODE_ITEMS = (('ISLAND', 'By Island', ''),
+                                 ('OVERLAP', 'By Overlap', ''))
+
+TD_SELECT_MODE_ITEMS = (('FACES_BY_TD', 'Faces (by Texel)', ''),
+                        ('ISLANDS_BY_TD', 'Islands (by Texel)', ''),
+						('ISLANDS_BY_SPACE', 'Islands (by UV Space)', ''))
+
+TD_SELECT_TYPE_ITEMS = (('EQUAL', 'Equal To', ''),
+                        ('LESS', 'Less Than', ''),
+                        ('GREATER', 'Greater Than', ''))
+
+
+PREFS_ANCHOR_ITEMS = (('LEFT_TOP', 'Left Top', ''),
+                      ('LEFT_BOTTOM', 'Left Bottom', ''),
+                      ('RIGHT_TOP', 'Right Top', ''),
+                      ('RIGHT_BOTTOM', 'Right Bottom', ''))
+
+PREFS_BACKEND_ITEMS = (('CPP', 'C++ (Fast)', ''), ('PY', 'Python (Slow)', ''))
+
+
+# Shaders
+VERTEX_SHADER_TEXT_3_0 = '''
+	in vec2 position;
+	out vec3 pos;
+
+	void main()
+	{
+		pos = vec3(position, 0.0f);
+		gl_Position = vec4(position, 0.0f, 1.0f);
+	}
+	'''
+
+FRAGMENT_SHADER_TEXT_3_0 = '''
+	uniform float pos_x_min;
+	uniform float pos_x_max;
+
+	in vec3 pos;
+
+	void main()
+	{
+		vec4 b = vec4(0.0f, 0.0f, 1.0f, 1.0f);
+		vec4 c = vec4(0.0f, 1.0f, 1.0f, 1.0f);
+		vec4 g = vec4(0.0f, 1.0f, 0.0f, 1.0f);
+		vec4 y = vec4(1.0f, 1.0f, 0.0f, 1.0f);
+		vec4 r = vec4(1.0f, 0.0f, 0.0f, 1.0f);
+
+		float pos_x_25 = (pos_x_max - pos_x_min) * 0.25 + pos_x_min;
+		float pos_x_50 = (pos_x_max - pos_x_min) * 0.5 + pos_x_min;
+		float pos_x_75 = (pos_x_max - pos_x_min) * 0.75 + pos_x_min;
+
+		float blendColor1 = (pos.x - pos_x_min)/(pos_x_25 - pos_x_min);
+		float blendColor2 = (pos.x - pos_x_25)/(pos_x_50 - pos_x_25);
+		float blendColor3 = (pos.x - pos_x_50)/(pos_x_75 - pos_x_50);
+		float blendColor4 = (pos.x - pos_x_75)/(pos_x_max - pos_x_75);
+
+		gl_FragColor = (c * blendColor1 + b * (1 - blendColor1)) * step(pos.x, pos_x_25) +
+						(g * blendColor2 + c * (1 - blendColor2)) * step(pos.x, pos_x_50) * step(pos_x_25, pos.x) +
+						(y * blendColor3 + g * (1 - blendColor3)) * step(pos.x, pos_x_75) * step(pos_x_50, pos.x) +
+						(r * blendColor4 + y * (1 - blendColor4)) * step(pos.x, pos_x_max) * step(pos_x_75, pos.x);
+	}
+	'''
+
+VERTEX_SHADER_TEXT_3_3 = '''
+	//in vec2 position;
+	//out vec3 pos;
+
+	void main()
+	{
+		pos = vec3(position, 0.0f);
+		gl_Position = vec4(position, 0.0f, 1.0f);
+	}
+	'''
+
+FRAGMENT_SHADER_TEXT_3_3 = '''
+	//uniform float pos_x_min;
+	//uniform float pos_x_max;
+
+	//in vec3 pos;
+
+	void main()
+	{
+		// Pure Colors
+		vec4 b = vec4(0.0f, 0.0f, 1.0f, 1.0f);	// Blue	0%
+		vec4 c = vec4(0.0f, 1.0f, 1.0f, 1.0f);	// Cyan	25%
+		vec4 g = vec4(0.0f, 1.0f, 0.0f, 1.0f);	// Green	50%
+		vec4 y = vec4(1.0f, 1.0f, 0.0f, 1.0f);	// Yellow	75%
+		vec4 r = vec4(1.0f, 0.0f, 0.0f, 1.0f);	// Red	100%
+
+		// Screen Space Coordinates for Intermediate Pure Colors
+		float pos_x_25 = (pos_x_max - pos_x_min) * 0.25 + pos_x_min;
+		float pos_x_50 = (pos_x_max - pos_x_min) * 0.5 + pos_x_min;
+		float pos_x_75 = (pos_x_max - pos_x_min) * 0.75 + pos_x_min;
+
+		// Intermediate Blend Values (0% - 25% => 0 - 1, 25% - 50% => 0 - 1, etc.)
+		float blendColor1 = (pos.x - pos_x_min)/(pos_x_25 - pos_x_min);
+		float blendColor2 = (pos.x - pos_x_25)/(pos_x_50 - pos_x_25);
+		float blendColor3 = (pos.x - pos_x_50)/(pos_x_75 - pos_x_50);
+		float blendColor4 = (pos.x - pos_x_75)/(pos_x_max - pos_x_75);
+
+		// Calculate Final Colors - Pure Colors and Blends between them 
+		FragColor = (c * blendColor1 + b * (1 - blendColor1)) * step(pos.x, pos_x_25) +
+						(g * blendColor2 + c * (1 - blendColor2)) * step(pos.x, pos_x_50) * step(pos_x_25, pos.x) +
+						(y * blendColor3 + g * (1 - blendColor3)) * step(pos.x, pos_x_75) * step(pos_x_50, pos.x) +
+						(r * blendColor4 + y * (1 - blendColor4)) * step(pos.x, pos_x_max) * step(pos_x_75, pos.x);
+	}
+	'''
---
+++ texel_density_2025_1_2/ui.py
@@ -0,0 +1,274 @@
+import bpy
+
+from . import utils
+from . import core_td_operators, add_td_operators, viz_operators
+from .constants import *
+
+def panel_draw(layout, context):
+	td = context.scene.td
+	is_view_3d_panel = context.area.type == 'VIEW_3D'
+
+	box = layout.box()
+	row = box.row(align=True)
+	row.operator(add_td_operators.ShowAddonPrefs.bl_idname, text="Prefs", icon='PREFERENCES')
+	row.operator(add_td_operators.OpenURL.bl_idname, text="Docs", icon='HELP').url = TD_DOC_URL
+	row.operator(add_td_operators.OpenURL.bl_idname, text="Report", icon='URL').url = TD_REPORT_URL
+
+	if context.active_object.type == 'MESH' and len(context.active_object.data.uv_layers) > 0:
+		box = layout.box()
+		row = box.row(align=True)
+		row.label(text="Units:")
+		row.prop(td, 'units', expand=False)
+		row = box.row(align=True)
+		row.label(text="Texture Size:")
+		row.prop(td, 'texture_size', expand=False)
+
+		# If custom texture size show fields for width and height
+		if td.texture_size == 'CUSTOM':
+			row = box.row(align=True)
+			row.label(text="Width:")
+			row.prop(td, "custom_width")
+			row.label(text=" px")
+
+			row = box.row(align=True)
+			row.label(text="Height:")
+			row.prop(td, "custom_height")
+			row.label(text=" px")
+
+		if is_view_3d_panel:
+			box = layout.box()
+			row = box.row(align=True)
+			row.label(text="Checker Method:")
+			row.prop(td, 'checker_method', expand=False)
+
+			row = box.row(align=True)
+			row.label(text="Checker Type:")
+			row.prop(td, 'checker_type', expand=False)
+
+			row = box.row(align=True)
+			row.label(text="UV Scale:")
+			row.prop(td, 'checker_uv_scale')
+
+			row = box.row()
+			row.operator(viz_operators.CheckerAssign.bl_idname, icon='MATSPHERE')
+
+			# If Checker Method "Store and Replace"
+			if td.checker_method == 'STORE':
+				row = box.row()
+				row.operator(viz_operators.CheckerRestore.bl_idname)
+				row = box.row()
+				row.operator(viz_operators.ClearSavedMaterials.bl_idname)
+
+		if context.object.mode == 'EDIT':
+			box = layout.box()
+			row = box.row()
+			if td.selected_faces:
+				row.prop(td, "selected_faces", text="Selected Faces", icon="CHECKBOX_HLT")
+			else:
+				row.prop(td, "selected_faces", text="Selected Faces", icon="CHECKBOX_DEHLT")
+
+		box = layout.box()
+		row = box.row(align=True)
+		row.label(text="UV Space:")
+		row.label(text=f"{td.uv_space} %")
+
+		cur_units = TD_UNITS_ITEMS[int(td.units)][1]
+
+		row = box.row(align=True)
+		row.label(text="Density:")
+		row.label(text=f"{td.density} {cur_units}")
+
+		row = box.row()
+		row.operator(core_td_operators.TexelDensityCheck.bl_idname, icon='EXPORT')
+		row = box.row()
+		row.operator(add_td_operators.CalculatedToSet.bl_idname, icon='COPYDOWN')
+		if context.object.mode == 'EDIT':
+			row = box.row()
+			row.operator(add_td_operators.CalculatedToSelect.bl_idname, icon='COPYDOWN')
+
+		box = layout.box()
+		row = box.row(align=True)
+		row.label(text="Set TD:")
+		row.prop(td, "density_set")
+		row.label(text=f" {cur_units}")
+
+		row = box.row(align=True)
+		row.label(text="Set Method:")
+		row.prop(td, 'set_method', expand=False)
+
+		row = box.row(align=True)
+		row.label(text="Scale Anchor:")
+		row.prop(td, 'rescale_anchor', expand=False)
+
+		row = box.row()
+		row.operator(core_td_operators.TexelDensitySet.bl_idname, icon='IMPORT')
+
+		# Preset Buttons
+		preset_rows = TD_PRESET_VALUES.get(td.units)
+		if preset_rows:
+			for preset_row in preset_rows:
+				row = box.row(align=True)
+				for val in preset_row:
+					op = row.operator(add_td_operators.PresetSet.bl_idname, text=val)
+					op.td_value = val
+
+		row = box.row(align=True)
+		row.operator(add_td_operators.PresetSet.bl_idname, text="Half TD", icon='FULLSCREEN_EXIT').td_value = "Half"
+		row.operator(add_td_operators.PresetSet.bl_idname, text="Double TD", icon='FULLSCREEN_ENTER').td_value = "Double"
+
+		if context.object.mode == 'OBJECT':
+			row = layout.row()
+			row.operator(add_td_operators.TexelDensityCopy.bl_idname, icon='UV_SYNC_SELECT')
+
+		if context.object.mode == 'EDIT':
+			box = layout.box()
+			row = box.row(align=True)
+			row.label(text="Select:")
+			row.prop(td, "select_mode", expand=False)
+
+			row = box.row(align=True)
+			row.label(text="Select Type:")
+			row.prop(td, "select_type", expand=False)
+
+			row = box.row(align=True)
+			if td.select_mode == "FACES_BY_TD" or td.select_mode == "ISLANDS_BY_TD":
+				row.label(text="Texel:")
+			elif td.select_mode == "ISLANDS_BY_SPACE":
+				row.label(text="UV Space:")
+
+			row.prop(td, "select_value")
+
+			if td.select_mode == "FACES_BY_TD" or td.select_mode == "ISLANDS_BY_TD":
+				row.label(text=" " + cur_units)
+			elif td.select_mode == "ISLANDS_BY_SPACE":
+				row.label(text=" %")
+
+			if td.select_type == "EQUAL":
+				row = box.row(align=True)
+				row.label(text="Select Threshold:")
+				row.prop(td, "select_threshold")
+
+			row = box.row()
+			if td.select_mode == "FACES_BY_TD":
+				row.operator(add_td_operators.SelectByTDOrUVSpace.bl_idname, text="Select Faces By TD", icon='ZOOM_SELECTED')
+			elif td.select_mode == "ISLANDS_BY_TD":
+				row.operator(add_td_operators.SelectByTDOrUVSpace.bl_idname, text="Select Islands By TD", icon='ZOOM_SELECTED')
+			elif td.select_mode == "ISLANDS_BY_SPACE":
+				row.operator(add_td_operators.SelectByTDOrUVSpace.bl_idname, text="Select Islands By UV Space", icon='ZOOM_SELECTED')
+
+		if is_view_3d_panel:
+			box = layout.box()
+			row = box.row(align=True)
+			row.label(text="Bake VC Mode:")
+			row.prop(td, "bake_vc_mode", expand=False)
+
+			if td.bake_vc_mode in {"TD_FACES_TO_VC", "TD_ISLANDS_TO_VC"}:
+				row = box.row()
+				if td.bake_vc_auto_min_max:
+					row.prop(td, "bake_vc_auto_min_max", text="Auto Min/Max Value", icon="CHECKBOX_HLT")
+				else:
+					row.prop(td, "bake_vc_auto_min_max", text="Auto Min/Max Value", icon="CHECKBOX_DEHLT")
+
+				row = box.row(align=True)
+				row.label(text="Min TD Value:")
+				row.label(text="Max TD Value:")
+
+				if td.bake_vc_auto_min_max:
+					row = box.row(align=True)
+					row.label(text=f"  {td.bake_vc_min_td}")
+					row.label(text=f"  {td.bake_vc_max_td}")
+				else:
+					row = box.row(align=True)
+					row.prop(td, "bake_vc_min_td")
+					row.prop(td, "bake_vc_max_td")
+
+			if td.bake_vc_mode == "UV_SPACE_TO_VC":
+				row = box.row(align=True)
+				row.label(text="Min UV Space:")
+				row.label(text="Max UV Space:")
+
+				row = box.row(align=True)
+				row.prop(td, "bake_vc_min_space")
+				row.prop(td, "bake_vc_max_space")
+
+			if td.bake_vc_mode == "UV_ISLANDS_TO_VC":
+				row = box.row(align=True)
+				row.label(text="Island Detect Mode:")
+				row.prop(td, "uv_islands_to_vc_mode", expand=False)
+
+			if td.bake_vc_mode == 'DISTORTION':
+				row = box.row(align=True)
+				row.label(text="Range:")
+				row.prop(td, "bake_vc_distortion_range")
+				row.label(text=" %")
+
+			if td.bake_vc_mode in {"TD_FACES_TO_VC", "TD_ISLANDS_TO_VC", "UV_SPACE_TO_VC", 'DISTORTION'}:
+				row = box.row()
+				if td.bake_vc_show_gradient:
+					row.prop(td, "bake_vc_show_gradient", text="Show Gradient", icon="CHECKBOX_HLT")
+				else:
+					row.prop(td, "bake_vc_show_gradient", text="Show Gradient", icon="CHECKBOX_DEHLT")
+
+			if td.bake_vc_mode in {"TD_FACES_TO_VC", "TD_ISLANDS_TO_VC"}:
+				row = box.row()
+				row.operator(viz_operators.BakeTDToVC.bl_idname, text="Texel Density to VC", icon='RESTRICT_RENDER_OFF')
+
+			if td.bake_vc_mode == "UV_SPACE_TO_VC":
+				row = box.row()
+				row.operator(viz_operators.BakeTDToVC.bl_idname, text="UV Space to VC", icon='RESTRICT_RENDER_OFF')
+
+			if td.bake_vc_mode == "UV_ISLANDS_TO_VC":
+				row = box.row()
+				row.operator(viz_operators.BakeTDToVC.bl_idname, text="UV Islands to VC", icon='RESTRICT_RENDER_OFF')
+
+			if td.bake_vc_mode == 'DISTORTION':
+				row = box.row()
+				row.operator(viz_operators.BakeTDToVC.bl_idname, text="UV Distortion to VC", icon='RESTRICT_RENDER_OFF')
+
+			row = box.row()
+			row.operator(viz_operators.ClearTDFromVC.bl_idname)
+
+	else:
+		if not context.active_object.type == 'MESH':
+			box = layout.box()
+			row = box.row(align=True)
+			row.label(text="Active object isn't a mesh", icon='ERROR')
+		elif len(context.active_object.data.uv_layers) == 0:
+			box = layout.box()
+			row = box.row(align=True)
+			row.label(text="Mesh doesn't have any UV", icon='ERROR')
+
+
+# Panel in 3D View
+class TDAddonView3DPanel(bpy.types.Panel):
+	bl_idname = "VIEW3D_PT_texel_density_checker"
+	bl_label = "Texel Density Checker 2025.1"
+	bl_space_type = "VIEW_3D"
+	bl_region_type = "UI"
+	bl_category = "Texel Density"
+
+	@classmethod
+	def poll(cls, context):
+		preferences = utils.get_preferences()
+		return (context.object is not None) and (context.active_object is not None) and preferences.view3d_panel_category_enable
+
+	def draw(self, context):
+		panel_draw(self.layout, context)
+
+
+# Panel in UV Editor
+class TDAddonUVPanel(bpy.types.Panel):
+	bl_idname = "UV_PT_texel_density_checker"
+	bl_label = "Texel Density Checker 2025.1"
+	bl_space_type = "IMAGE_EDITOR"
+	bl_region_type = "UI"
+	bl_category = "Texel Density"
+
+	@classmethod
+	def poll(cls, context):
+		preferences = utils.get_preferences()
+		return ((context.object is not None) and (context.active_object is not None) and context.mode == 'EDIT_MESH' and preferences.uv_panel_enable
+				and context.space_data.mode == 'UV')
+
+	def draw(self, context):
+		panel_draw(self.layout, context)
